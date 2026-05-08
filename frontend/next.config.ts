19456718b27d1c7ef3f8b558e9cde3eb103b7fe0
Nookie31247 <dudanmam@gmail.com>
2026-05-08 16:43:01 +0900

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,23 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    return[
+    return [
       {
         source: '/api/:path*',
         destination: 'http://internal-fullstack-alb-backend-821091328.ap-northeast-2.elb.amazonaws.com:8080/api/:path*',
+      },
+    ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'X-Forwarded-Host',
+            value: 'nookie-server.store',
+          },
+        ],
       },
     ]
   },
